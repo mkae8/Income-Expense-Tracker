c6c9e98f863965c9b9df45dcf82b3d6264999aec
Bilguun <Bilguun040728@gmail.com>
2024-09-27 18:26:25 +0800

    hh

--- client/src/components/dash/Chartt.jsx
+++ client/src/components/dash/Chartt.jsx
@@ -1,22 +1,7 @@
-// "use client";
-
-// import { Bar } from "react-chartjs-2";
-
-// export const Chartt = () => {
-//   const asd = {
-//     labels: [2016, 2017],
-//     datasets: [],
-//   };
-
-//   return (
-//     <div className="w-52 h-52 flex">
-//       <Bar data={asd} />
-//     </div>
-//   );
-// };
+/** @format */
 
 "use client";
-
+import React from "react";
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -40,11 +25,11 @@ ChartJS.register(
 
 export const Chartt = () => {
   const data = {
-    labels: [2016, 2017],
+    labels: [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
     datasets: [
       {
         label: "# of Votes",
-        data: [12, 19],
+        data: [12, 19, 21, 2],
         backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
         borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
         borderWidth: 1,
@@ -53,7 +38,7 @@ export const Chartt = () => {
   };
 
   return (
-    <div className="w-52 h-52 flex">
+    <div className="w-fit h-52 px-[50px] pl-8 flex">
       <Bar
         data={data}
         options={{
--- server/utils/constant.js
+++ server/utils/constant.js
@@ -1,4 +1,4 @@
 /** @format */
 
 export const DbPath =
-  "/Users/24LP0612/Desktop/income-and-expence-tracker/server/utils/db.json";
+  "/Users/bilguundavaadorj/income-and-expence-tracker/server/utils/db.json";
